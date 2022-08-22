b2856efdb3b03a3e0bdf058cf5a563d0e9c9c07e
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-08-21 22:05:26 -0700

    connect the psql

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,18 +10,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudent() {
 
-        return List.of(
-                new Student(
-                        1L,
-                        "Maria",
-                        "maria@gmail.com",
-                        13,
-                        LocalDate.of(2002, Month.JANUARY, 12)
-
-                )
-
-        );
+        return studentRepository.findAll();
     }
 }
