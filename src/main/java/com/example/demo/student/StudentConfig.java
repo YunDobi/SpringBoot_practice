d60ab551ae4a3c693bbbd54917d5747288c92053
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-08-22 01:39:36 -0700

    insert the data to the table

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student marin = new Student(
+                    "Marin",
+                    "Marin12@gmail.com",
+                    21,
+                    LocalDate.of(2000, JANUARY, 23)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "Alex@gmail.com",
+                    21,
+                    LocalDate.of(2003, FEBRUARY, 22)
+            );
+
+            repository.saveAll(
+                    List.of(marin,alex)
+            );
+        };
+    }
+}
